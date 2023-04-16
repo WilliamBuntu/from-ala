fd2e34218a992d4726d9aa12242998c0d3d931ea
Buntu William <williambuntu1@gmail.com>
2023-04-17 01:50:47 +0200

    commit

---
+++ sandbox.js
@@ -0,0 +1,8 @@
+function reveal() {
+    document.getElementById("div").style.height = "100%"
+    document.getElementById("div").style.width = "100%"
+}
+function hide() {
+    document.getElementById("div").style.height = "0%"
+    document.getElementById("div").style.width = "0%"
+}
